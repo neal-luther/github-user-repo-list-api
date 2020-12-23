33fc34988cd469eafa02210181db108b5974cfff
g00dvibe <53553854+neal-luther@users.noreply.github.com>
2020-12-22 20:27:09 -0800

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,43 @@
 'use strict'
+
+const searchURL = 'https://api.github.com/users/';
+//---------------------------------------------
+function displayResults(responseJson) {
+    console.log(responseJson);
+    $('#results-list').empty();//clears list for new search
+    for (let i = 0; i < responseJson.length; i++){//iterate through repos and add to list
+        $('#results-list').append(
+            `<li><a href="${responseJson[i].html_url}">${responseJson[i].name}</a>
+            <p>${responseJson[i].description}</p>
+            </li>`
+        )
+    }
+    $('#results').removeClass('hidden');
+}
+//---------------------------------------------
+function getRepos(searchTerm) {
+    const url = searchURL + searchTerm + "/repos";
+
+    fetch(url)
+        .then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error(response.statusText);
+        })
+        .then(responseJson => displayResults(responseJson))
+        .catch(err => {
+            $('#js-error-message').text(`Something went wrong: ${err.message}`);
+            $('#results-list').empty();
+        });
+    }
+//---------------------------------------------
+function watchForm() {
+    $('form').submit(event => {
+        event.preventDefault();
+        const searchTerm = $('#js-search-term').val();
+        getRepos(searchTerm);
+    });
+}
+//---------------------------------------------
+$(watchForm);
